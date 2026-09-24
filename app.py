dda6e32cb0377e2cbc322b12dfb8148d0c45cfc5
Huzaifa Sheikh <huzaifasheikh90999@gmail.com>
2026-09-25 02:26:16 +0500

    Fix API URL scheme

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # --
-API_URL = "librarybackend-pqvr34ctr-huzaifa-sheikh-s-projects.vercel.app" 
+API_URL = "https://librarybackend-pqvr34ctr-huzaifa-sheikh-s-projects.vercel.app"
 
 st.set_page_config(page_title="Library Management", layout="centered")
 st.title("Library Management System")
